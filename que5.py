6bea5a9b7fa78f745199969b606faca0402e6149
ved-007 <tvedant7777@gmail.com>
2025-07-30 15:18:13 +0530

    que5.py

--- que5.py
+++ que5.py
@@ -7,7 +7,7 @@ from scipy.stats import pearsonr , chi2_contingency
 
 sns.set(style="whitegrid")
 
-df =pd.read_csv("/home/vedant/Downloads/demographic_data.csv")
+df =pd.read_csv("Resources/demographic_data.csv")
 
 plt.figure(figsize=(10,6))
 sns.histplot(data=df,x="Age",y="Education Level",multiple="dodge",palette="Set2",bins=10,legend=False,hue="Education Level")
